487e5059bf7b70832d7674e425daade46d8353b4
Persis Ratouis <persis.ratouis@gmail.com>
2018-02-16 15:31:07 -0800

    added some CSS and started changes to todolist

--- reactApp/app.js
+++ reactApp/app.js
@@ -5,24 +5,11 @@ const dummyData = ['eat a hamburger in Sapporo',
 'email Cyrus Samii', 'email Teymour', 'email Mary Joe',
 'mail to Meghann', 'mail to Rebecca' , 'letter to Gretchen',
 'letter to Alexa (DC)', 'phone to Firouz', 'ask Reema about flights'];
+const myTasks = dummyData.map((task) => {
+	return {'taskText': task, 'completed': false};
+})
 
-class Todo extends React.Component {
-  render() {
-    return ( /*<div style={{display: 'inline'}}>*/
-    <li>
-    <button onClick={()=>null}>X</button>{this.props.task}
-  </li> );
-  }
-}
-
-class TodoList extends React.Component {
-  render() {
-    //style={{display: flex, flexDirection: column}}
-    return(<ul>
-      {dummyData.map((task) => <Todo task={task}/>)}
-    </ul>);
-  }
-}
+myTasks[4].completed = true;
 
 class InputLine extends React.Component {
   constructor(props){
@@ -42,20 +29,57 @@ class InputLine extends React.Component {
   }
 
   render() {
-    return( <form onSubmit={(e) => this.handleSubmit(e)} style={{display: inline}}>
-      <input type="text"
+    return( <form onSubmit={(e) => this.handleSubmit(e)} style={{display: 'inline'}}>
+      <input className="newTask" type="text"
              onChange={(e)=> this.handleTaskInputChange(e)}
-             placeholder="eat portabello burger in Sapporo..."/>
-      <input type="submit" value="add"/>
+             placeholder="write task here..."/>
+      <input type="submit" value="add" className="completed"/>
     </form>);
   }
 }
 
+class Todo extends React.Component {
+  render() {
+    return ( /*<div style={{display: 'inline'}}>*/
+    <li className="todoItem">
+    <button className="completed" onClick={()=>null}>X</button>
+    {this.props.completed ?
+      <strike>this.props.task</strike> :
+      this.props.task
+    }
+  </li> );
+  }
+}
+
+class TodoList extends React.Component {
+  render() {
+    return(<ul>
+      {this.props.todos.map((task) => <Todo task={task.taskText} completed={task.completed}/>)}
+    </ul>);
+  }
+}
+
+
 class TodoApp extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {todos: []}
+  }
+
+  componentDidMount(){
+    this.setState({todos: myTasks});
+  }
+
   render(){
     return(<div>
       <h3>Persis' Todo App</h3>
-      <TodoList />
+      <div className="inputline">
+      <InputLine/>
+      </div>
+      <div className="todo">
+        <TodoList todos={this.state.todos}/>
+      </div>
+
     </div>)
   }
 }
