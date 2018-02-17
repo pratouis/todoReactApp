ea6748d086f613f60d47a15653270cfb233cec99
Persis Ratouis <persis.ratouis@gmail.com>
2018-02-16 16:59:44 -0800

    including mongoose in backend

--- backend/routes/databaseAccess.js
+++ backend/routes/databaseAccess.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const TodoItem = require('../models/TodoItem');
 
 router.get('/add', (req, res) => {
-  res.send('Hello');
+  const testTodo = new TodoItem({
+    task: "test task"
+  });
+
+  testTodo.save()
+      .then(response => res.send(response))
+      .catch(error => res.status(500).send(error));
 });
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app = express();
 const dbRoutes = require('./routes/databaseAccess.js');
 
+const mongoose = require('mongoose');
+// Ensure that there is a MONGODB_URI environment variable (source env.sh)
+if (!process.env.MONGODB_URI) {
+  throw new Error("MONGODB_URI is not in the environmental variables. Try running 'source env.sh'");
+}
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on('connected', function() {
+  console.log('Success: connected to MongoDb!');
+});
+mongoose.connection.on('error', function(err) {
+  console.log('Error connecting to MongoDb: ' + err);
+  process.exit(1);
+});
+
 app.use('/db',dbRoutes);
 app.use(express.static('build'));
 
